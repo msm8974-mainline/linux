b324b1180141daaa2e0048c7a3a513c6814b49cf
Rob Clark <robdclark@gmail.com>
2015-09-22 20:24:08 -0400

    hack: allow vram carveout for gpu

--- drivers/gpu/drm/msm/adreno/a3xx_gpu.c
+++ drivers/gpu/drm/msm/adreno/a3xx_gpu.c
@@ -513,8 +513,8 @@ struct msm_gpu *a3xx_gpu_init(struct drm_device *dev)
 		 * implement a cmdstream validator.
 		 */
 		DRM_DEV_ERROR(dev->dev, "No memory protection without IOMMU\n");
-		ret = -ENXIO;
-		goto fail;
+//		ret = -ENXIO;
+//		goto fail;
 	}
 
 	/*
