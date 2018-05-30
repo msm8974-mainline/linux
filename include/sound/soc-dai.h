7c32ba85ab478659f72958a65ef4d8cda3910ff3
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2018-05-30 15:29:54 +0100

    ASoC: core: add support to dai get_channel_map
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

--- include/sound/soc-dai.h
+++ include/sound/soc-dai.h
@@ -141,6 +141,11 @@ int snd_soc_dai_set_tristate(struct snd_soc_dai *dai, int tristate);
 int snd_soc_dai_digital_mute(struct snd_soc_dai *dai, int mute,
 			     int direction);
 
+
+int snd_soc_dai_get_channel_map(struct snd_soc_dai *dai,
+         unsigned int *tx_num, unsigned int *tx_slot,
+         unsigned int *rx_num, unsigned int *rx_slot);
+
 int snd_soc_dai_is_dummy(struct snd_soc_dai *dai);
 
 struct snd_soc_dai_ops {
@@ -169,6 +174,10 @@ struct snd_soc_dai_ops {
 		unsigned int tx_num, unsigned int *tx_slot,
 		unsigned int rx_num, unsigned int *rx_slot);
 	int (*set_tristate)(struct snd_soc_dai *dai, int tristate);
+  	int (*get_channel_map)(struct snd_soc_dai *dai,
+                unsigned int *tx_num, unsigned int *tx_slot,
+                 unsigned int *rx_num, unsigned int *rx_slot);
+
 
 	int (*set_sdw_stream)(struct snd_soc_dai *dai,
 			void *stream, int direction);
