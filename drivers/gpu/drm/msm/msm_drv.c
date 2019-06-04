b93b8017dad5ffd4ba5e7402bbbc15e754c0b104
Brian Masney <masneyb@onstation.org>
2019-06-04 06:58:44 -0400

    HACK: init vram first
    Signed-off-by: Brian Masney <masneyb@onstation.org>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -432,14 +432,14 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 
 	drm_mode_config_init(ddev);
 
+	ret = msm_init_vram(ddev);
+	if (ret)
+		goto err_msm_uninit;
+
 	/* Bind all our sub-components: */
 	ret = component_bind_all(dev, ddev);
 	if (ret)
 		goto err_destroy_mdss;
-
-	ret = msm_init_vram(ddev);
-	if (ret)
-		goto err_msm_uninit;
 
 	msm_gem_shrinker_init(ddev);
 
