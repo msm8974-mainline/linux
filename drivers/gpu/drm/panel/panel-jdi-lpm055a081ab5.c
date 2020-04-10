aa3ebd660fd5793b59721576a53caf6d9fddd1da
Luca Weiss <luca@z3ntu.xyz>
2020-04-10 21:35:31 +0200

    panel: lpm055a081ab5: add drm_connector argument to get_modes()

--- drivers/gpu/drm/panel/panel-jdi-lpm055a081ab5.c
+++ drivers/gpu/drm/panel/panel-jdi-lpm055a081ab5.c
@@ -316,7 +316,8 @@ static const struct drm_display_mode default_mode = {
 	.flags = 0,
 };
 
-static int jdi_panel_get_modes(struct drm_panel *panel)
+static int jdi_panel_get_modes(struct drm_panel *panel,
+			       struct drm_connector *connector)
 {
 	struct drm_display_mode *mode;
 	struct jdi_panel *jdi = to_jdi_panel(panel);
@@ -332,10 +333,10 @@ static int jdi_panel_get_modes(struct drm_panel *panel)
 
 	drm_mode_set_name(mode);
 
-	drm_mode_probed_add(panel->connector, mode);
+	drm_mode_probed_add(connector, mode);
 
-	panel->connector->display_info.width_mm = 61;
-	panel->connector->display_info.height_mm = 110;
+	connector->display_info.width_mm = 61;
+	connector->display_info.height_mm = 110;
 
 	return 1;
 }
