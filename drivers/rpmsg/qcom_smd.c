16e13f34f18d3dc0e34cc941b08e531ec1845ea3
Luca Weiss <luca@z3ntu.xyz>
2024-04-15 21:22:07 +0200

    FROMLIST v1 rpmsg: qcom_smd: Don't print error during probe deferral
    When the mailbox driver has not probed yet, skip printing the error
    message since it's just going to confuse users.
    
    Signed-off-by: Luca Weiss <luca@z3ntu.xyz>

--- drivers/rpmsg/qcom_smd.c
+++ drivers/rpmsg/qcom_smd.c
@@ -1502,7 +1502,8 @@ struct qcom_smd_edge *qcom_smd_register_edge(struct device *parent,
 
 	ret = qcom_smd_parse_edge(&edge->dev, node, edge);
 	if (ret) {
-		dev_err(&edge->dev, "failed to parse smd edge\n");
+		if (ret != -EPROBE_DEFER)
+			dev_err(&edge->dev, "failed to parse smd edge\n");
 		goto unregister_dev;
 	}
 
